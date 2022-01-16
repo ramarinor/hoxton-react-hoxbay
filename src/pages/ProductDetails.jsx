67d9145847c3ee4bb8be11749f344e7d361c3a23
Rinor Rama <rinor.rama95@gmail.com>
2022-01-16 13:27:43 +0100

    rendered ProductDetails Page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,13 +1,42 @@
-import { useParams } from 'react-router-dom'
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
-function ProductDetails () {
-  const params = useParams()
+function ProductDetails() {
+  const params = useParams();
+  const [product, setProduct] = useState(null);
 
-  // fetch product
+  useEffect(() => {
+    fetch(`http://localhost:3000/products/${params.id}`)
+      .then((resp) => resp.json())
+      .then((productFromServer) => setProduct(productFromServer));
+  }, []);
 
-  console.log('Params', params)
+  console.log("Params", params);
 
-  return <h1>Details for product: {params.id}</h1>
+  if (product === null) {
+    return <main>Loading....</main>;
+  }
+  if (product.title === undefined) {
+    return <main>Product not found!</main>;
+  }
+  return (
+    <main>
+      <section className="product-detail main-wrapper">
+        <img src={product.image} alt={product.title} />
+
+        <div
+          className="product-detail__side"
+          style={{ borderColor: "var(--yellow)" }}
+        >
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>£{product.price}</p>
+          <button>Add to basket</button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
